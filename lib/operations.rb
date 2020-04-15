4fc2b6558a4e199bd1842d8765cf3e417abaf4f8
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-15 22:42:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,26 +1,7 @@
-def unsafe?(speed = "79")
-  
-  
-  if speed > 60
-  end
-    
-  if speed < 40
-  end
-      
-  if speed 40 < 60
+def unsafe?(speed)
+
 end
 
-unsafe?("60")
+def not_safe?(speed)
 
-
-def not_safe?(speed = "35")
-  speed > 60 ? "true" : "false"
 end
-  
-  speed < 40 ? "true" : "false"
-end
-  
-  speed 40 < 60 "true" : "false"
-end
-
-not_safe?("70")
